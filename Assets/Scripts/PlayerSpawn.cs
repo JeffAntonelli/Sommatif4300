0467b09ae76bd381ba1d8f3284f4bc9271079277
JeffAntonelli <antonellijeffrey@gmail.com>
2021-04-18 19:50:36 +0200

    Add pause menu whit restart of level, Dontdestroyonload

---
+++ Assets/Scripts/PlayerSpawn.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawn : MonoBehaviour
+{
+    private void awake()
+    {
+        GameObject.FindGameObjectWithTag("Player").transform.position = transform.position;
+    }
+}
