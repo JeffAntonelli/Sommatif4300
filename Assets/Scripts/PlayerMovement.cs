13ccebb690dbe0bfbf9d9c26c03a0a9d410cd053
LudoBernard <ludo.bernard.pro@gmail.com>
2021-04-19 18:40:13 +0200

    Changed the animations, character now faces the direction he walked to

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,20 +11,31 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Animator animator_;
 
 
-    private Vector2 movement;
+    private Vector2 movement_;
+    private Vector2 lastMovement_;
     // Update is called once per frame
     private void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        movement_.x = Input.GetAxisRaw("Horizontal");
+        movement_.y = Input.GetAxisRaw("Vertical");
         
-        animator_.SetFloat("Horizontal", movement.x);
-        animator_.SetFloat("Vertical", movement.y);
-        animator_.SetFloat("Speed", movement.sqrMagnitude);
+        if (movement_.x < -0.5f || movement_.x > 0.5f) {
+            lastMovement_ = new Vector2(movement_.x, 0.0f);
+        }
+        if (movement_.y < -0.5f || movement_.y > 0.5f) {
+            lastMovement_ = new Vector2(0.0f, movement_.y);
+        }
+        
+        
+        animator_.SetFloat("Horizontal", movement_.x);
+        animator_.SetFloat("Vertical", movement_.y);
+        animator_.SetFloat("Speed", movement_.sqrMagnitude);
+        animator_.SetFloat("LastMovementX", lastMovement_.x);
+        animator_.SetFloat("LastMovementY", lastMovement_.y);
     }
 
     private void FixedUpdate()
     {
-        rb_.MovePosition(rb_.position + movement * (moveSpeed_ * Time.fixedDeltaTime));
+        rb_.MovePosition(rb_.position + movement_ * (moveSpeed_ * Time.fixedDeltaTime));
     }
 }
